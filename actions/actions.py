db315169f5b767fce7aad68c1311f26de0bfd2f3
harsh <harsh6100@gmail.com>
2019-08-19 10:02:11 +0000

    fry

---
+++ actions/actions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+ import sys 
+ import requests as req
+ 
+ from st2common.runners.base_action import Action class MyEchoAction(Action):
+
+    def run(self, url):
+		resp = req.get(url)
+		print(resp.status_code)
+		print(resp.url)
